fdd8304c5427ba18549ed59f0865b00fad48df44
Rahul <r-vb@users.noreply.github.com>
2023-03-17 23:50:00 +0530

    Version 2.9.4

--- solutions/b.c
+++ solutions/b.c
@@ -27,21 +27,41 @@ return 0;
 void read(struct BOOK arr[], int num_books)
 {
         int i;
-    printf("\nEnter details %d books--\n",num_books);
+    printf("\nEnter details of %d bookss--\n",num_books);
     for ( i = 0; i < num_books; i++)
     {
-        printf("Enter TITLE: ");
-        scanf("%s",&arr->title);
-    }
-    
+        printf("TITLE of Book %d: ",i+1);
+        scanf("%s",arr[i].title);
+        printf("Enter EDITION: ");
+        scanf("%d",&arr[i].edition);
+        printf("Enter PRICE: ");
+        scanf("%d",&arr[i].price);
+    }    
 }
 
 void display(struct BOOK arr[], int num_books)
 {
-
+        int i;
+    printf("\nDetails of %d books--\n",num_books);
+    for ( i = 0; i < num_books; i++)
+    {
+        printf("Book Title %d: %s\n",i+1,arr[i].title);
+        printf("Edition: %d\n",arr[i].edition);
+        printf("Price: %d\n",arr[i].price);
+    }    
 }
 
 void compute(struct BOOK arr[], int num_books)
-{
-
+{        int i=0, lar;
+    lar = i;
+    for (i = 1; i < num_books; i++){
+            if (arr[lar].price <= arr[i].price){
+                    lar = i;
+            }            
+    }
+    printf("\n\nDetails of Book worth highest price is:\n");
+        printf("Book %d details-\n",lar+1);
+        printf("Title: ");      printf("%s\n",arr[lar].title);
+        printf("Edition: ");    printf("%d\n",arr[lar].edition);
+        printf("Price: ");      printf("%d\n",arr[lar].price);
 }
