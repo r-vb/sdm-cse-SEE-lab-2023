5f91f206442f96745e87089095bad9a9ca6683c8
Rahul <r-vb@users.noreply.github.com>
2023-03-23 19:18:48 +0530

    Version 5.2.6

---
+++ solutions/h.c
@@ -0,0 +1,76 @@
+/* ascending priority queue */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct node* head = NULL;
+
+void insert(int data) {
+    struct node* new_node = (struct node*) malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = NULL;
+    
+    if (head == NULL || head->data >= data) {
+        new_node->next = head;
+        head = new_node;
+    } else {
+        struct node* current = head;
+        while (current->next != NULL && current->next->data < data) {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+}
+
+void delete() {
+    if (head == NULL) {
+        printf("Queue is empty.\n");
+    } else {
+        struct node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+void print() {
+    struct node* current = head;
+    printf("Queue: ");
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\n1. Insert\n2. Delete\n3. Print\n4. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter the value to be inserted: ");
+                scanf("%d", &value);
+                insert(value);
+                break;
+            case 2:
+                delete();
+                break;
+            case 3:
+                print();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice.\n");
+        }
+    }
+    return 0;
+}
